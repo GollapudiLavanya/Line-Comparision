1baecdc8b1744e0bd111dd901f295afceb8a29c9
GollapudiLavanya <86302821+GollapudiLavanya@users.noreply.github.com>
2021-06-30 15:49:22 +0530

    Update Program.cs
    changed a line

--- Program.cs
+++ Program.cs
@@ -14,10 +14,10 @@ namespace Line_Comparision_check_equality
             Console.WriteLine("Enter values x2 and y2 for Line 1");
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x1 and y1 for Line 2");
+            Console.WriteLine("Enter values x3 and y3 for Line 2");
             int x3 = Convert.ToInt32(Console.ReadLine());
             int y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x2 and y2 for Line 2");
+            Console.WriteLine("Enter values x4 and y4 for Line 2");
             int x4 = Convert.ToInt32(Console.ReadLine());
             int y4 = Convert.ToInt32(Console.ReadLine());
             double length_of_line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
